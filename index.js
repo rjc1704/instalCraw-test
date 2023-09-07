4415a1899cb78917f78fc8cd8fe720b6708e015d
Sunwoo <dlatjsdn205@Naver.com>
2023-09-07 22:40:39 +0900

    Fix: server fix

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const express = require('express')
 const app = express();
 const port = 3000;
 
-app.get("/get", async(req, res) => {
-  const browser = await puppeteer.launch({headless:false});
+app.get("/getInstaData", async(req, res) => {
+  const browser = await puppeteer.launch({headless:'new'});
   const page = await browser.newPage();
-  
-  await page.goto(req.url, { waitUntil: "networkidle0" });
+  let url = req.query.url
+  await page.goto(url, { waitUntil: "networkidle0" });
 
   const imgEl = await page.$(
     "main > div > div > article > div > div > div > div > div > div > img"
