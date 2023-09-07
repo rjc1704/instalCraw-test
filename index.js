7c469db3a0b9c5c3805d8573fa06aded09c326ae
Sunwoo <dlatjsdn205@Naver.com>
2023-09-07 23:53:31 +0900

    Chore: cors setting

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-const puppeteer = require('puppeteer')
-
-const express = require('express')
+const puppeteer = require('puppeteer');
+const express = require('express');
 const app = express();
-const port = 4000;
+const cors = require('cors');
+const port = 4001;
+app.use(cors());
 
-app.get("/", async(req, res) => {
-  const browser = await puppeteer.launch({headless:'new'});
-  const page = await browser.newPage();
-  let url = req.query.url
-  console.log(req, url)
-  await page.goto(url, { waitUntil: "networkidle0" });
+app.get('/', async (req, res) => {
+const browser = await puppeteer.launch({ headless: 'new' });
+const page = await browser.newPage();
+let url = req.query.url;
+await page.goto(url, { waitUntil: 'networkidle0' });
 
-  const imgEl = await page.$(
-    "main > div > div > article > div > div > div > div > div > div > img"
-  )
+const imgEl = await page.$('main > div > div > article > div > div > div > div > div > div > img');
 
-  const imgSrc = await page.evaluate((img) => img.src, imgEl)
-  
-  await browser.close();
-  console.log(imgSrc)
-  res.send({imgSrc, hashtags:"#13.7챌린지"})
-})
+const imgSrc = await page.evaluate((img) => img.src, imgEl);
+
+await browser.close();
+
+res.send(imgSrc);
+});
 
 app.listen(port, () => {
-  console.log(`listening ${port}`)
-})
+console.log(`listening ${port}`);
+});
