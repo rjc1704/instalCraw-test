47141047213d211fc4ae841b2d98ab3e14234ad3
Sunwoo <dlatjsdn205@Naver.com>
2023-09-08 01:09:00 +0900

    fix: test2

--- index.js
+++ index.js
@@ -5,19 +5,24 @@ const cors = require('cors');
 const port = 4001;
 app.use(cors());
 
-app.get('/', async (req, res) => {
-const browser = await puppeteer.launch({ headless: 'new' });
-const page = await browser.newPage();
-let url = req.query.url;
-await page.goto(url, { waitUntil: 'networkidle0' });
-
-const imgEl = await page.$('main > div > git div > article > div > div > div > div > div > div > img');
-
-const ImageUrl = await page.evaluate((img) => img.src, imgEl);
-
-await browser.close();
-
-res.send({ImageUrl, hashtags:"#13.7챌린지"});
+app.get('/', (req, res) => {
+  
+  const url = req.query.url;
+  async function get(url) {
+    try {
+      const browser = await puppeteer.launch({ headless: 'new' });
+      const page = await browser.newPage();
+      await page.goto(url, { waitUntil: 'networkidle0' });
+      const imgEl = await page.$('main > div > git div > article > div > div > div > div > div > div > img');
+      const ImageUrl = await page.evaluate((img) => img.src, imgEl);
+      await browser.close();
+      res.send({ImageUrl, hashtags:"#13.7챌린지"});
+    } catch (error) {
+      console.log(error)
+      res.status(500).send(error.message);
+    }
+  }
+  get(url)
 });
 
 app.listen(port, () => {
